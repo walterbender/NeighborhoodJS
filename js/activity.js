26500eef7a728c3b316573e0655f582a359acef5
Walter Bender <walter@sugarlabs.org>
2014-10-24 18:46:22 -0400

    new project

---
+++ js/activity.js
@@ -0,0 +1,131 @@
+// Copyright (c) 2014 Walter Bender
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+
+define(function (require) {
+    var activity = require('sugar-web/activity/activity');
+    var icon = require('sugar-web/graphics/icon');
+    require('easel');
+    require('activity/turtles');
+
+    // Manipulate the DOM only when it is ready.
+    require(['domReady!'], function (doc) {
+
+        // Initialize the activity.
+        activity.setup();
+
+        // Colorize the activity icon.
+        var activityButton = docById('activity-button');
+        var colors;  // I should be getting the XO colors here?
+        activity.getXOColor(function (error, colors) {
+            icon.colorize(activityButton, colors);
+        });
+
+	//
+        var canvas = docById('myCanvas');
+	var stage;
+	var turtles;
+
+	// default values
+	var defaultBackgroundColor = [70, 80, 20];
+
+        // Make the activity stop with the stop button.
+        var stopButton = docById('stop-button');
+        stopButton.addEventListener('click', function (e) {
+            activity.close();
+        });
+
+	// Do we need to update the stage?
+        var update = true;
+
+	// Get things started
+	init();
+
+	function docById(id) {
+	    return document.getElementById(id);
+	}
+
+	function last(myList) {
+	    var i = myList.length;
+	    if (i == 0) {
+		return null;
+	    } else {
+		return myList[i - 1];
+	    }
+	}
+
+        function init() {
+	    self.dict = {}
+
+            docById('loader').className = 'loader';
+
+            // Check to see if we are running in a browser with touch support.
+            stage = new createjs.Stage(canvas);
+	    turtles = new Turtles(canvas, stage, refreshCanvas);
+
+	    createjs.Ticker.addEventListener('tick', tick);
+
+            // Enable touch interactions if supported on the current device.
+            createjs.Touch.enable(stage);
+            // Keep tracking the mouse even when it leaves the canvas.
+            stage.mouseMoveOutside = true;
+            // Enabled mouse over and mouse out events.
+            stage.enableMouseOver(10); // default is 20
+
+	    initNeighborhood();
+	}
+
+	function initNeighborhood() {
+	    // this comes from somewhere
+	    var names = ['tincho', 'gonzalo', 'rgs', 'rms', 'walter', 'patty'];
+	    for (var i in names) {
+		addTurtle(names[i]);
+	    }
+	}
+
+	function addTurtle(name) {
+	    if (self.dict[name]) {
+		self.dict[name].container.visible = true;
+	    } else {
+		turtles.add(name);
+		self.dict[name] = last(turtles.turtleList);
+	    }
+	    self.dict[name].container.x = Math.floor(Math.random() * (canvas.width));
+	    self.dict[name].container.y = Math.floor(Math.random() * (canvas.height));
+	    update = true;
+	}
+
+	function removeTurtle(name) {
+	    if (self.dict[name]) {
+		self.dict[name].container.visible = false;
+	    }
+	    update = true;
+	}
+
+        function stop() {
+	    //
+            createjs.Ticker.removeEventListener('tick', tick);
+        }
+
+	function refreshCanvas() {
+	    update = true;
+	}
+
+        function tick(event) {
+            // This set makes it so the stage only re-renders when an
+            // event handler indicates a change has happened.
+            if (update) {
+                update = false; // Only update once
+                stage.update(event);
+            }
+        }
+    });
+
+});
---
+++ js/turtles.js
@@ -0,0 +1,113 @@
+// Copyright (c) 2014 Walter Bender
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+
+// Turtles
+var defaultColor = 5;
+var defaultValue = 50;
+var defaultChroma = 100;
+var defaultStroke = 5;
+
+// Turtle sprite
+var turtlePath = 'images/turtle.svg';
+var turtleBasePath = 'images/';
+
+function Turtle (name, turtles) {
+    this.name = name;
+    this.turtles = turtles;
+
+    console.log(name);
+    // Things used for drawing the turtle.
+    this.container = null;
+    this.bitmap = null;
+};
+
+function Turtles(canvas, stage, refreshCanvas) {
+    this.canvas = canvas;
+    this.stage = stage;
+    this.refreshCanvas = refreshCanvas;
+    
+    // The list of all of our turtles, one for each start block.
+    this.turtleList = [];
+
+    this.add = function(name) {
+	// Add a new turtle for each start block
+	console.log('adding a new turtle ' + name);
+	var i = this.turtleList.length;
+	var turtleName = i.toString();
+	var myTurtle = new Turtle(name, this);
+	this.turtleList.push(myTurtle);
+
+	// Each turtle needs its own canvas.
+        myTurtle.drawingCanvas = new createjs.Shape();
+        this.stage.addChild(myTurtle.drawingCanvas);
+
+	var turtleImage = new Image();
+	i %= 10;
+	turtleImage.src = turtleBasePath + 'turtle-' + i.toString() + '.svg';
+	myTurtle.container = new createjs.Container();
+	this.stage.addChild(myTurtle.container);
+	myTurtle.bitmap = new createjs.Bitmap(turtleImage);
+	myTurtle.container.addChild(myTurtle.bitmap);
+	myTurtle.container.x = myTurtle.x;
+	myTurtle.container.y = myTurtle.y;
+	myTurtle.bitmap.x = 0;
+	myTurtle.bitmap.y = 0;
+	myTurtle.bitmap.regX = 27 | 0;
+	myTurtle.bitmap.regY = 27 | 0;
+	myTurtle.bitmap.name = 'bmp_turtle';
+	myTurtle.bitmap.cursor = 'pointer';
+	var hitArea = new createjs.Shape();
+	hitArea.graphics.beginFill('#FFF').drawEllipse(-27, -27, 55, 55);
+	hitArea.x = 0;
+	hitArea.y = 0;
+	myTurtle.container.hitArea = hitArea;
+
+        this.stage.update();
+
+	var turtles = this;
+
+	myTurtle.container.on('click', function(event) {
+	    console.log(myTurtle.name);
+	    alert(myTurtle.name);
+	});
+
+	myTurtle.container.on('mousedown', function(event) {
+	    var offset = {
+                x: myTurtle.container.x - event.stageX,
+                y: myTurtle.container.y - event.stageY
+	    }
+
+	    myTurtle.container.on('pressmove', function(event) {
+                myTurtle.container.x = event.stageX + offset.x;
+                myTurtle.container.y = event.stageY + offset.y;
+		myTurtle.x = myTurtle.container.x;
+		myTurtle.y = myTurtle.container.y;
+                turtles.refreshCanvas();
+	    });
+	});
+
+	myTurtle.container.on('mouseover', function(event) {
+            myTurtle.bitmap.scaleX = 1.2;
+            myTurtle.bitmap.scaleY = 1.2;
+            myTurtle.bitmap.scale = 1.2;
+            turtles.refreshCanvas();
+        });
+
+	myTurtle.container.on('mouseout', function(event) {
+            myTurtle.bitmap.scaleX = 1;
+            myTurtle.bitmap.scaleY = 1;
+            myTurtle.bitmap.scale = 1;
+            turtles.refreshCanvas();
+        });
+
+        this.refreshCanvas();
+    }
+}
